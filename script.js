51f7877531007ae05bc6efba55a8766793b7f1fd
fannr <fatahnoer398@gmail.com>
2022-05-03 01:39:03 +0100

    revisi 3

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const scrollTop = document.querySelector(".scrollTop");
+
 // when browser load
 window.addEventListener("load", function () {
   // Animate for Navbar
@@ -20,42 +22,29 @@ window.addEventListener("load", function () {
 });
 
 // Nav-link when click hide menu
-const navA = document.querySelectorAll(".nav-link");
-navA.forEach((a) => {
-  a.addEventListener("click", function () {
-    document.querySelector(".navbar-collapse").classList.remove("show");
-  });
-});
-// Nav-link when click hide menu
 
 window.addEventListener("scroll", function () {
   const navbar = document.querySelector(".navbar");
   let scrollPos = this.scrollY;
 
-  if (scrollPos > 0) {
+  if (this.scrollY > 50) {
     navbar.classList.add("scrollUp");
     navbar.classList.add("shadow-sm");
+    scrollTop.classList.add("active");
   } else {
+    scrollTop.classList.remove("active");
     navbar.classList.remove("scrollUp");
     navbar.classList.remove("shadow-sm");
   }
 
-  // Icon fadeIn scroll Top
-  const homeSection = document.querySelector(".home");
-  const scrollTop = document.querySelector(".scrollTop");
-  if (scrollPos < homeSection.offsetTop + homeSection.offsetHeight - 100) {
-    scrollTop.classList.remove("active");
-  } else {
-    scrollTop.classList.add("active");
-  }
   // Icon fadeIn scroll Top
 
   // Active class Nav-link
   const section = document.querySelectorAll("section");
   section.forEach((s) => {
     if (
-      scrollPos >= s.offsetTop - 100 &&
-      scrollPos < s.offsetTop + s.offsetHeight - 100
+      this.scrollY >= s.offsetTop - 100 &&
+      this.scrollY < s.offsetTop + s.offsetHeight - 100
     ) {
       let currendId = s.attributes.id.value;
       removeAllActiveClass();
@@ -79,7 +68,7 @@ window.addEventListener("scroll", function () {
 
   // Animate Category element
   const category = document.querySelector(".category");
-  if (scrollPos >= category.offsetTop - 150) {
+  if (this.scrollY >= category.offsetTop - 150) {
     const elCategory = document.querySelectorAll("#animateCategory");
     elCategory.forEach((c, i) => {
       setTimeout(() => {
@@ -91,7 +80,7 @@ window.addEventListener("scroll", function () {
 
   // Animate Article Title ~ 1
   const titleContent1 = document.querySelector(".titleContent1");
-  if (scrollPos >= titleContent1.offsetTop - 250) {
+  if (this.scrollY >= titleContent1.offsetTop - 250) {
     const elArticle = document.querySelectorAll("#animateArticle");
     elArticle.forEach((c, i) => {
       setTimeout(() => {
@@ -103,7 +92,7 @@ window.addEventListener("scroll", function () {
 
   // Animate Article Title ~ 2
   const titleContent2 = document.querySelector(".titleContent2");
-  if (scrollPos >= titleContent2.offsetTop - 250) {
+  if (this.scrollY >= titleContent2.offsetTop - 250) {
     const elArticle2 = document.querySelectorAll("#animateArticle2");
     elArticle2.forEach((c, i) => {
       setTimeout(() => {
@@ -115,7 +104,7 @@ window.addEventListener("scroll", function () {
 
   // Animate Article Title ~ 3
   const titleContent3 = document.querySelector(".titleContent3");
-  if (scrollPos >= titleContent3.offsetTop - 200) {
+  if (this.scrollY >= titleContent3.offsetTop - 200) {
     const elArticle3 = document.querySelectorAll("#animateArticle3");
     elArticle3.forEach((c, i) => {
       setTimeout(() => {
@@ -126,8 +115,7 @@ window.addEventListener("scroll", function () {
   // Animate Article Title ~ 3
 
   // Animate Contact
-  const contact = document.querySelector(".contact");
-  if (scrollPos >= contact.offsetTop - 450) {
+  if (this.scrollY >= titleContent3.offsetTop - 250) {
     const elContact = document.querySelectorAll("#animateContact");
     elContact.forEach((el, i) => {
       setTimeout(() => {
@@ -136,4 +124,23 @@ window.addEventListener("scroll", function () {
     });
   }
   // Animate Contact
+});
+
+// Nav-link when click hide menu
+const navA = document.querySelectorAll(".link");
+navA.forEach((a) => {
+  a.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+    document.querySelector(".navbar-collapse").classList.remove("show");
+  });
+});
+
+scrollTop.addEventListener("click", function (e) {
+  e.preventDefault();
+  document.querySelector(this.getAttribute("href")).scrollIntoView({
+    behavior: "smooth",
+  });
 });
